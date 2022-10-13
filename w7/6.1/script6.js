88909f254ff3dd97605c09d03498be932c5dd6eb
StaceyPeters01 <9018900@student.zadkine.nl>
2022-10-13 14:18:46 +0200

    6.1

---
+++ w7/6.1/script6.js
@@ -0,0 +1,11 @@
+function ChangeColor(){
+alert("De kleur wordt aangepast.")
+console.log("Het werkt ook in de console!")
+document.getElementById("tekst1").style.color = "red"
+}
+
+function Calculator(num,num2){
+    let answer = num + num;
+    console.log(answer);
+    alert(answer);
+}
